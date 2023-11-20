d2d0953bc74a5eb777231837b7513cb805c849d8
Adson Ventura <adsonventura@gmail.com>
2023-11-20 18:09:54 -0300

    Adiciona eslint da rocketseat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { Heading } from "@ignite-ui/react";
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return (
-    <Heading>Opa eae</Heading>
-  )
+  return <Heading>Opa eae</Heading>
 }
