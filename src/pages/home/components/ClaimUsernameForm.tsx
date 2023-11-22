ed47b0c16945c5e2a3e074f5767c7f767416a464
Adson Ventura <adsonventura@gmail.com>
2023-11-22 09:28:05 -0300

    Adiciona form de usuario

---
+++ src/pages/home/components/ClaimUsernameForm.tsx
@@ -0,0 +1,17 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Form } from './styles'
+
+export function ClaimUsernameForm() {
+  return (
+    <Form as="form">
+      {/* eslint-disable-next-line */}
+      {/* @ts-ignore */}
+      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
