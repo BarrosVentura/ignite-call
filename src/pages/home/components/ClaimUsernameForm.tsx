184fb6eeaf6fa619c35ea74949c4ef3165a7b923
Adson Ventura <adsonventura@gmail.com>
2023-11-22 09:34:07 -0300

    Adiciona hook form e zod para inicio de validação de formulario

--- src/pages/home/components/ClaimUsernameForm.tsx
+++ src/pages/home/components/ClaimUsernameForm.tsx
@@ -1,13 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       {/* eslint-disable-next-line */}
       {/* @ts-ignore */}
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
