de400227834b618f8f22e9f1e0cb65061969e09c
Adson Ventura <adsonventura@gmail.com>
2024-01-12 08:43:36 -0300

    Adiciona estrutura inicial do componente calendar

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '../../../lib/prisma'
 import { Container, UserHeader } from './styles'
+import { ScheduleForm } from './ScheduleForm'
 
 interface ScheduleProps {
   user: {
@@ -19,6 +20,8 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UserHeader>
+
+      <ScheduleForm />
     </Container>
   )
 }
