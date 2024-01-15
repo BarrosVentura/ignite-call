02103b72b9f9d63d5d498ccd1656097cc12c8a3e
Adson Ventura <adsonventura@gmail.com>
2024-01-15 15:08:39 -0300

    Adiciona navegação entre meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,22 +8,42 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import dayjs from 'dayjs'
+import { useState } from 'react'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonth = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonth)
+  }
+
+  function handleNextMonth() {
+    const nextMonth = currentDate.add(1, 'month')
+
+    setCurrentDate(nextMonth)
+  }
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Mês anterior">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Mês seguinte">
             <CaretRight />
           </button>
         </CalendarActions>
